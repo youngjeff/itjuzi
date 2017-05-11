2a83474a7b9fa905cf05eca7585310d921d23695
“your_username <your_registered_github_Email>
2017-05-11 20:10:16 +0800

    asd

--- itjuzi_dis/pipelines.py
+++ itjuzi_dis/pipelines.py
@@ -48,30 +48,30 @@ class ItjuziSpiderPipeline(object):
             company.company_status=item['company_status']
             company.info_id = item['info_id']
             session.add(company)
-            if item['tz_info']:
-                for touzi in item['tz_info']:
-                    tz = JuziTz()
-                    tz.company_id = company.info_id
-                    tz.tz_time = touzi['tz_time']
-                    tz.tz_finades = touzi['tz_finades']
-                    tz.tz_capital = touzi['tz_capital']
-                    tz.tz_round = touzi['tz_round']
-                    session.add(tz)
-            if item['tm_info']:
-                for team in item['tm_info']:
-                    tm = JuziTeam()
-                    tm.company_id = company.info_id
-                    tm.tm_m_name = team['tm_m_name']
-                    tm.tm_m_title = team['tm_m_title']
-                    tm.tm_m_intro = team['tm_m_intro']
-                    session.add(tm)
-            if item['pdt_info']:
-                for product in item['pdt_info']:
-                    pdt = JuziProduct()
-                    pdt.company_id = company.info_id
-                    pdt.pdt_name = product['pdt_name']
-                    pdt.pdt_type = product['pdt_type']
-                    pdt.pdt_intro = product['pdt_intro']
-                    session.add(pdt)
-            session.commit()
+            # if item['tz_info']:
+            #     for touzi in item['tz_info']:
+            #         tz = JuziTz()
+            #         tz.company_id = company.info_id
+            #         tz.tz_time = touzi['tz_time']
+            #         tz.tz_finades = touzi['tz_finades']
+            #         tz.tz_capital = touzi['tz_capital']
+            #         tz.tz_round = touzi['tz_round']
+            #         session.add(tz)
+            # if item['tm_info']:
+            #     for team in item['tm_info']:
+            #         tm = JuziTeam()
+            #         tm.company_id = company.info_id
+            #         tm.tm_m_name = team['tm_m_name']
+            #         tm.tm_m_title = team['tm_m_title']
+            #         tm.tm_m_intro = team['tm_m_intro']
+            #         session.add(tm)
+            # if item['pdt_info']:
+            #     for product in item['pdt_info']:
+            #         pdt = JuziProduct()
+            #         pdt.company_id = company.info_id
+            #         pdt.pdt_name = product['pdt_name']
+            #         pdt.pdt_type = product['pdt_type']
+            #         pdt.pdt_intro = product['pdt_intro']
+            #         session.add(pdt)
+            # session.commit()
         return item
