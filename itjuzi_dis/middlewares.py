80b47f2cba22a0d667a1877130e4b2aa1efe7280
“your_username <your_registered_github_Email>
2017-05-11 19:15:36 +0800

    hahah

---
+++ itjuzi_dis/middlewares.py
@@ -0,0 +1,57 @@
+# coding:utf8
+
+import logging
+import random
+
+# Start your middleware class
+
+from scrapy.contrib.downloadermiddleware.useragent import UserAgentMiddleware
+
+
+class RotateUserAgentMiddleware(UserAgentMiddleware):
+    def __init__(self, user_agent=''):
+        self.user_agent = user_agent
+
+    def process_request(self, request, spider):
+        # 这句话用于随机选择user-agent
+        ua = random.choice(self.user_agent_list)
+        if ua:
+            logging.info(ua)
+            request.headers.setdefault('User-Agent', ua)
+
+    # the default user_agent_list composes chrome,I E,firefox,Mozilla,opera,netscape
+    # for more user agent strings,you can find it in http://www.useragentstring.com/pages/useragentstring.php
+        user_agent_list = [
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36",
+                "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
+                "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
+                "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0",
+                "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; InfoPath.3; rv:11.0) like Gecko",
+                "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0;"
+                "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+                "Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+                "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11",
+                "Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+                "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)",
+                "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)"
+            ]
+
+
+
+
+# class ProxyMiddleware(object):
+#     # overwrite process request
+#     def process_request(self, request, spider):
+#         # Set the location of the proxy
+#         sql = 'select ip,port from t_proxy_ip t where t.is_valid =1'
+#         result = SqlUtil.query_all(sql)
+#         ip_port = random.choice(result)
+#         logging.info(ip_port)
+#         request.meta['proxy'] = "http://{0}:{1}".format(ip_port['ip'], ip_port['port'])
+#         # # Use the following lines if your proxy requires authentication
+#         # proxy_user_pass = "USERNAME:PASSWORD"
+#         # # setup basic authentication for the proxy
+#         # encoded_user_pass = base64.encodestring(proxy_user_pass)
+#         # request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
